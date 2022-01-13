9c72a7586942bfd41d9d33185d4df6d8d6745c16
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-13 23:26:33 +0600

    CSS in JS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+      {/* <h1 style={{ color: "red", backgroundColor: "black" }}>{title}</h1>  */}
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   );
 };
@@ -15,6 +16,12 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string,
   // title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+const headingStyle = {
+  color: "white",
+  backgroundColor: "blue",
 };
 
 export default Header;
