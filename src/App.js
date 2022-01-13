6ef2951d20d68254da921723c6424728ef72a1d9
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-13 20:53:54 +0600

    Class Based Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-function App() {
-  // const name = "Dewan";
-  // const x = true;
+import React from "react"; // required for class based component
 
-  return (
-    <div className="container">
-      <h1>Hello from React!</h1>
-      {/* <h2>Hello {name}</h2> */}
-      {/* <h2>{x ? "Yes" : "No"}</h2> */}
-    </div>
-  );
+// import Header from "./components/Header";
+
+// function App() {
+//   // const name = "Dewan";
+//   // const x = true;
+
+//   return (
+//     <div className="container">
+//       {/* <h1>Hello from React!</h1> */}
+//       {/* <h2>Hello {name}</h2> */}
+//       {/* <h2>{x ? "Yes" : "No"}</h2> */}
+//       <Header />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a Class!</h1>;
+  }
 }
 
 export default App;
