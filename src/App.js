334fb378c0676cd74b55c8d39524f1671633647f
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-13 20:59:39 +0600

    Props and Default Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React from "react"; // required for class based component
+import Header from "./components/Header";
 
-// import Header from "./components/Header";
-
-// function App() {
-//   // const name = "Dewan";
-//   // const x = true;
-
-//   return (
-//     <div className="container">
-//       {/* <h1>Hello from React!</h1> */}
-//       {/* <h2>Hello {name}</h2> */}
-//       {/* <h2>{x ? "Yes" : "No"}</h2> */}
-//       <Header />
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a Class!</h1>;
-  }
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
